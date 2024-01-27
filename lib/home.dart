16345ff3fe39663c81e4808e19cbece0444c2c2e
Manoj Prabhakar <manojptn16@gmail.com>
2024-01-27 11:26:13 +0530

    Initial commit

---
+++ lib/home.dart
@@ -0,0 +1,117 @@
+import 'package:demo/register.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(250),
+        child: AppBar(
+          backgroundColor: Colors.red[100],
+          flexibleSpace: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Image widget
+              Image.asset(
+                'assets/1.png',
+                width: 200, // adjust the width as needed
+                height: 100, // adjust the height as needed
+              ),
+              // Three Text widgets with different font sizes
+              Text(
+                'Welcome',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                '"Unlock Your Journey"',
+                style: TextStyle(fontSize: 18),
+              ),
+              Text(
+                'Register Today',
+                style: TextStyle(fontSize: 18),
+              ),
+              // SizedBox for additional spacing if needed
+            ],
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Center(
+              child: Text(
+                'Login',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Enter Your Phone Number',
+                border: OutlineInputBorder(),
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.orange, width: 2.0),
+                ),
+                contentPadding: EdgeInsets.all(16.0),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Expanded(
+                  child: Divider(
+                    color: Colors.orange,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    'OR',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Expanded(
+                  child: Divider(
+                    color: Colors.orange,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            GestureDetector(
+              onTap: () {
+                // Navigate to the next screen
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NextScreen()),
+                );
+              },
+              child: Center(
+                child: Text(
+                  'Register An New Account ?',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {
+                // Handle button press
+              },
+              child: Text('Continue'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/register.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NextScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Next Screen'),
+      ),
+      body: Center(
+        child: Text('welcome to transmaa'),
+      ),
+    );
+  }
+}
